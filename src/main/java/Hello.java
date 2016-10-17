09bd5beb031501ef02eef5db65c0405568866f1d
Somaiah Kumbera <somaiah.kumbera@capgemini.com>
2016-10-17 18:23:28 +0200

    init

---
+++ src/main/java/Hello.java
@@ -0,0 +1,15 @@
+/**
+ * User: skumbera
+ * Date: 17.10.16
+ * Time: 17:10
+ */
+public class Hello {
+
+    public String greeting() {
+        return "Hello";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Hello().greeting());
+    }
+}
